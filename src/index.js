73b65ae7e6b7d159f0dbaa509d99c03c099f6bff
Guillermo Izalde <izalde.guillermo45@gmail.com>
2023-03-31 00:23:40 -0600

    proyecto a empaquetar

---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+
+const data = {
+    name: "Guillermo Izalde",
+    nickname: "Guillermo Izalde",
+    description: "...",
+    avatar: "...",
+    social: [
+      {
+        name: "faceebok",
+        url: "https://twitter.com/",
+        username: "",
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/",
+        username: "",
+      },
+    ],
+    links: [
+      {
+        name: "blog",
+        url: "https://example.com/",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://example.com/",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Mexico",
+  };
